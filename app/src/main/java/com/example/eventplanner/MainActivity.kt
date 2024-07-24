e45854000855fbd2a73889adfd229da490c62bc5
santn <kevinsan@edu.aau.at>
2024-07-24 19:46:22 +0200

    Added Navigation

--- app/src/main/java/com/example/eventplanner/MainActivity.kt
+++ app/src/main/java/com/example/eventplanner/MainActivity.kt
@@ -6,10 +6,10 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
 import com.example.eventplanner.ui.theme.EventPlannerTheme
 
 class MainActivity : ComponentActivity() {
@@ -21,7 +21,13 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    MainScreen()
+                    val navController = rememberNavController()
+                    NavHost(navController, startDestination = "home") {
+                        composable("home") { MainScreen(navController) }
+                        composable("addEvent") { AddEventScreen() }
+                        composable("myEvents") { MyEventsScreen() }
+                        composable("calendar") { CalendarScreen() }
+                    }
                 }
             }
         }
